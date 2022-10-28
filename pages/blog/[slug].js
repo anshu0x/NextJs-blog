a8d5da35b6e2a964d1670f3ac5b8d37adce4a556
anshu0x <anshusharma6327@gmail.com>
2022-10-28 18:50:38 +0530

    dangerouslySetInnerHTML

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,10 +1,15 @@
 import React from "react";
 const slug = ({ data }) => {
+  function createMarkup() {
+    return {__html: data.html};
+  }
+  
   return (
     <div className="container w-50 my-4">
       <p>Title : {data?.title}</p>
       <p>Content : {data?.content}</p>
       <p>Author : {data?.author}</p>
+      <div dangerouslySetInnerHTML={createMarkup()}></div>
     </div>
   );
 };
@@ -13,6 +18,7 @@ export default slug;
 
 export async function getServerSideProps(context) {
   // Fetch data from external API
+  console.log(context);
   const res = await fetch(
     `http://localhost:3000/api/blog?slug=${context.query.slug}`
   );
